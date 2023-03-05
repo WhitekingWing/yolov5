46f958e32757f81055642135737de5530dac52da
WhitekingWing <98199553+WhitekingWing@users.noreply.github.com>
2023-03-05 16:27:48 +0800

    Update train.py
    Signed-off-by: WhitekingWing <98199553+WhitekingWing@users.noreply.github.com>

--- train.py
+++ train.py
@@ -435,7 +435,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default=ROOT / 'yolov5s.pt', help='initial weights path')
+    parser.add_argument('--weights', type=str, default=ROOT / 'best.pt', help='initial weights path')
     parser.add_argument('--cfg', type=str, default=ROOT / 'models/yolov5n_data.yaml', help='model.yaml path')
     parser.add_argument('--data', type=str, default=ROOT / 'data/data.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
